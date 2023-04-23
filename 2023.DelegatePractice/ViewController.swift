c4fc586a77afd5164ee12b57da507dee9c728114
Tomtom <Tomtom_Chu@everrich.com.tw>
2023-04-23 16:39:29 +0800

    新增專案 移除storyboard、scenedelegate，增加gitignore檔案

---
+++ 2023.DelegatePractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  2023.DelegatePractice
+//
+//  Created by 曲奕帆 on 2023/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+        
+    }
+
+
+}
+
